2b6ee2d07d728eab74ee23574fcc4ad856fe0011
HT16A087 <ht16a087@oecu.jp>
2019-04-03 04:55:17 +0900

    delete pods

--- Pods/Target Support Files/Pods-ReminderApp/Pods-ReminderApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReminderApp : NSObject
-@end
-@implementation PodsDummy_Pods_ReminderApp
-@end
--- Pods/Target Support Files/Pods-ReminderAppUITests/Pods-ReminderAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReminderAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ReminderAppUITests
-@end
